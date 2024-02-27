b14a9d49cdd9d375d92fcb912c2f3ef747365536
Molly - Webstruxure <molly@webstruxure.co.nz>
2024-02-27 14:10:48 +1300

    first push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8664c92394c245d0a329eaa5744dd25a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5974cfbd-2a22-4029-9255-71bff110d716'
+};
