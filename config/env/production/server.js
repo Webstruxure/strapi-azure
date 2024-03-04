0bdbdc1b014574cc489f8f833b85c29c236f32be
Molly - Webstruxure <molly@webstruxure.co.nz>
2024-03-05 11:37:18 +1300

    server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: 'https://strapi-webapp-new.azurewebsites.net',
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+  port: process.env.PORT || env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8664c92394c245d0a329eaa5744dd25a'),
+    },
+    cors: {
+      origin: ['*'],
+    },
   },
-  serveAdminPanel: false
 });
+
+const strapi = require('@strapi/strapi')
+strapi().start()
