8f77f80bfaecdf70f3c47af97ec64e0c45f216f6
Molly - Webstruxure <molly@webstruxure.co.nz>
2024-03-05 10:52:18 +1300

    yml

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,13 +1,9 @@
 module.exports = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    url: 'https://strapi-webapp-new.azurewebsites.net',
-  });
-
-module.exports = ({ env }) => ({
-auth: {
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: 'https://strapi-webapp-new.azurewebsites.net',
+  auth: {
     secret: env('ADMIN_JWT_SECRET'),
-},
-url: '/',
-serveAdminPanel: false
+  },
+  serveAdminPanel: false
 });
