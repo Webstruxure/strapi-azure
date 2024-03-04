cfde1edd7188aaecb1b1ed16e6afa76ccd2e9100
Molly - Webstruxure <molly@webstruxure.co.nz>
2024-03-05 12:05:10 +1300

    npm

--- config/database.js
+++ config/database.js
@@ -1,14 +1,35 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', ''),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
--- server.js
+++ server.js
@@ -11,5 +11,5 @@ module.exports = ({ env }) => ({
   },
 });
 
-const strapi = require('@strapi/strapi')
-strapi().start()
+// const strapi = require('@strapi/strapi')
+// strapi().start()
