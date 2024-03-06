dc9351d3c3fd961095052c8e9dc34c8d3401bee3
Molly - Webstruxure <molly@webstruxure.co.nz>
2024-03-06 13:17:26 +1300

    Custom index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    module: {
-      rules: [
+    entry: '/config/server.js', 
+    output: [
         {
-          test: /\.woff2$/,
-          use: 'null-loader',
+            filename: 'main.4af98f3d.js',
+            path: path.resolve(__dirname, 'build')
         },
-      ],
-    },
-  };
-  
+        {
+            filename: 'runtime~main.7c1f9226.js',
+            path: path.resolve(__dirname, 'build')
+        }
+    ]
+};
